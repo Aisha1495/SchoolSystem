6c4293ac3d41852ccf85408e1246cecc2ce4da43
Aisha1495 <aishaalmamari6@gmail.com>
2022-12-25 02:39:51 -0800

    add index

--- src/aisha/ExecutionClass.java
+++ src/aisha/ExecutionClass.java
@@ -16,6 +16,7 @@ public class ExecutionClass {
 //		Marks mark = new Marks();
 //		Courses cours = new Courses();
 		List<Department> departmentList = new ArrayList<>();
+		int depListIndex=0;
 		boolean addDepartment = true;
 
 		Scanner sc = new Scanner(System.in);
@@ -27,27 +28,34 @@ public class ExecutionClass {
 		while (addDepartment) {
 
 			Department dep = new Department();
-
+			departmentList.add(dep);
 			System.out.println("Enter Department Name:  ");
 			String depName = sc.next();
-			dep.setNameOfDep(depName);
+			departmentList.get(depListIndex).setNameOfDep(depName);
+			//dep.setNameOfDep(depName);
+			
+			
 
 			System.out.println("Enter Department id:  ");
 			int depId = sc.nextInt();
-			dep.setDepNum(depId);
+			departmentList.get(depListIndex).setDepNum(depId);
+			//dep.setDepNum(depId);
 
+			int techListIndex = 0;
 			boolean addTeacher = true;
 			while (addTeacher) {
 
 				Teacher tech = new Teacher();
-
+				departmentList.get(depListIndex).TeacherList.add(tech);
 				System.out.println("Enter Teacher Name:  ");
 				String techName = sc.next();
-				tech.setTeachName(techName);
+				departmentList.get(depListIndex).TeacherList.get(techListIndex).setTeachName(techName);
+				//tech.setTeachName(techName);
 
 				System.out.println("Enter Teacher id:  ");
 				int techId = sc.nextInt();
-				tech.setTeachId(techId);
+				departmentList.get(depListIndex).TeacherList.get(techListIndex).setTeachId(techId);
+				//tech.setTeachId(techId);
 
 				System.out.println("Do you want to Enter Other Teacher? (yes/no):  ");
 				String answer1 = sc.next();
@@ -55,26 +63,34 @@ public class ExecutionClass {
 				if (answer1.equals("yes")) {
 					System.out.println("Next Teacher:");
 					addTeacher = true;
+					techListIndex++;
+					
+				
 
 				} else if (answer1.equals("no")) {
 					addTeacher = false;
 					System.out.println("Student:");
 					break;
 
-				}
 			}
+			}
+			
 
+			int studListIndex = 0;
 			boolean addStudent = true;
 			while (addStudent) {
 				Student stud = new Student();
+				departmentList.get(depListIndex).TeacherList.get(techListIndex).StudList.add(stud);
 
 				System.out.println("Enter Student Name:  ");
 				String studName = sc.next();
-				stud.setStudName(studName);
+				departmentList.get(depListIndex).TeacherList.get(techListIndex).StudList.get(studListIndex).setStudName(studName);
+				//stud.setStudName(studName);
 
 				System.out.println("Enter Student id:  ");
 				int studId = sc.nextInt();
-				stud.setStudId(studId);
+				departmentList.get(depListIndex).TeacherList.get(techListIndex).StudList.get(studListIndex).setStudId(studId);
+				//stud.setStudId(studId);
 
 				System.out.println("Do you want to Enter Other Student? (yes/no):  ");
 				String answer2 = sc.next();
@@ -82,27 +98,36 @@ public class ExecutionClass {
 				if (answer2.equals("yes")) {
 					System.out.println("Next Student:");
 					addStudent = true;
+					
+					studListIndex++;
 
 				} else if (answer2.equals("no")) {
 					addStudent = false;
 					System.out.println("Course:");
 					break;
+					
+					
 
 				}
+				}
 
-			}
+			
 
+			int coursListIndex =0;
 			boolean addCourse = true;
 			while (addCourse) {
 				Courses cour = new Courses();
+				departmentList.get(depListIndex).TeacherList.get(techListIndex).StudList.get(studListIndex).CoursList.add(cour);
 
 				System.out.println("Enter Course Name:  ");
 				String courName = sc.next();
-				cour.setCourName(courName);
+				departmentList.get(depListIndex).TeacherList.get(techListIndex).StudList.get(studListIndex).CoursList.get(coursListIndex).setCourName(courName);
+				//cour.setCourName(courName);
 
 				System.out.println("Enter Course id:  ");
 				int courId = sc.nextInt();
-				cour.setCourId(courId);
+				departmentList.get(depListIndex).TeacherList.get(techListIndex).StudList.get(studListIndex).CoursList.get(coursListIndex).setCourId(courId);
+				//cour.setCourId(courId);
 
 				System.out.println("Do you want to Enter Other Course? (yes/no):  ");
 				String answer3 = sc.next();
@@ -110,6 +135,8 @@ public class ExecutionClass {
 				if (answer3.equals("yes")) {
 					System.out.println("Next Course:");
 					addCourse = true;
+					coursListIndex++;
+				
 
 				} else if (answer3.equals("no")) {
 					addCourse = false;
@@ -119,9 +146,11 @@ public class ExecutionClass {
 				}
 
 			}
+			int markListIndex=0;
 			boolean addMark = true;
 			while (addMark) {
 				Marks mark = new Marks();
+				departmentList.get(depListIndex).TeacherList.get(techListIndex).StudList.get(studListIndex).CoursList.get(coursListIndex).markList.add(mark);
 
 				System.out.println("Enter three supject:  ");
 
@@ -133,7 +162,8 @@ public class ExecutionClass {
 				System.out.println(subN_3);
 
 				System.out.println("Enter Three Marks of Each Mark");
-				mark.markAvrg();
+				departmentList.get(depListIndex).TeacherList.get(techListIndex).StudList.get(studListIndex).CoursList.get(coursListIndex).markList.get(markListIndex).markAvrg();
+				//mark.markAvrg();
 
 				System.out.println("Do you want to add new Mark? yes/no");
 				String answer4 = sc.next();
@@ -141,24 +171,29 @@ public class ExecutionClass {
 				if (answer4.equals("yes")) {
 					System.out.println("Next Student Mark:");
 					addMark = true;
+					markListIndex++;
+					
+				
 				} else if (answer4.equals("no")) {
 					addMark = false;
 					System.out.println("end:");
 					break;
 
-				}
-
-			}
+				
+				}}
+			
 			departmentList.add(dep);
 
 			System.out.println("Do you want to add new department? yes/no");
 			String answer10 = sc.next();
 			if (answer10.equals("yes")) {
 				addDepartment = true;
+				depListIndex++;
 				System.out.println("Next Department");
 			} else if (answer10.equals("no")) {
 				addDepartment = false;
-
+			
+			
 				// break;
 				System.out.println("Thanks");
 				System.out.println("**************************");
@@ -212,5 +247,6 @@ public class ExecutionClass {
 				}
 			}
 		}
-	}
-}
+	
+	}}
+	
